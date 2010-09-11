f1a0cedea151cd5e9188461327f9eec56b1304ad
Matt Windsor <mattwindsor@btinternet.com>
2010-09-11 13:09:51 +0100

    curl invocation is now silent.

--- main.c
+++ main.c
@@ -1,31 +1,30 @@
 /**************************************************************************
  *                                                                        *
  *  statustran                                                            *
- *                                                                        *
+o *                                                                        *
  *  StatusNet translator for the GNU operating system (requires curl)     *
  *                                                                        *
  *  Copyright 1995, 1996, 1997, 1998, 1999, 2001, 2002, 2007              *
  *            Free Software Foundation, Inc.                              *
  *                                                                        *
  *  Copyright 2010  Matt Windsor <hayashi@archhurd.org>                   *
- *                                                                        *
+ *                                                                        * 
  *  Based on work by Gordon Matzigkeit <gord@fig.org>                     *
  *               and Miles Bader       <miles@gnu.org>                    *
+ *                                                                        * 
  *                                                                        *
- *  This file is part of statustran.                                      *
- *                                                                        *
- *  statustran is free software: you can redistribute it and/or modify    *
+ *  This program is free software: you can redistribute it and/or modify  *
  *  it under the terms of the GNU General Public License as published by  *
  *  the Free Software Foundation, either version 3 of the License, or     *
  *  (at your option) any later version.                                   *
  *                                                                        *
- *  statustran is distributed in the hope that it will be useful,         *
+ *  This program is distributed in the hope that it will be useful,       *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *  GNU General Public License for more details.                          *
  *                                                                        *
  *  You should have received a copy of the GNU General Public License     *
- *  along with statustran.  If not, see <http://www.gnu.org/licenses/>.   *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. *
  *                                                                        *
  **************************************************************************/
 
@@ -271,7 +270,7 @@ trivfs_S_io_write (trivfs_protid_t cred,
     {
       execl ("/bin/curl", "/bin/curl", "-u", userauth,
 	     URL, "-d", ebuf, "-d", ORIGIN_STANZA, 
-	     "-o", "/dev/null", NULL);
+	     "-s", NULL);
       _exit (0);
     }
 
